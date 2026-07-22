b8ea0560000390690d1e39cab769e39e67a0723b
Abbasfd05 <Abbasfadhel2005abfd@gmail.com>
2026-07-22 15:31:03 +0300

    comment

--- app.js
+++ app.js
@@ -27,4 +27,18 @@ const greet = () => {
   let message = "Hello, " + friendName + "!";
   console.log(message);
 }
-greet()
+greet();
+let a = 4;
+
+const foo = (x) => {
+  let b = a * 4;
+
+  const bar = (y) => {
+    let c = y * b;
+    return c;
+  }
+
+  return bar(b);
+}
+
+console.log(foo(a));
